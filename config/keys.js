f23037a8c90269c546652e416568096de9effb59
Simon Colman <s_colman@hotmail.com>
2020-04-19 10:27:46 +1000

    deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI || "mongodb+srv://harsh:usyd2020@facebooklogin-imntv.mongodb.net/test?retryWrites=true&w=majority" ,
+  mongoURI: process.env.MONGODB_URI || "mongodb://<user1><password1>@ads125871.mlab.com25871/heroku_0"
+  // "mongodb+srv://harsh:usyd2020@facebooklogin-imntv.mongodb.net/test?retryWrites=true&w=majority" ,
   secretOrKey: "secret"
 };
